e0a3e431615662f164666b9d34315d2176241786
maxaqquepucho <maxaqquepucho@gmail.com>
2018-06-17 00:52:27 -0500

    ahora ya se puede agregar filas a la tabla

---
+++ js/CRUD.js
@@ -0,0 +1,39 @@
+
+let tabla = document.querySelector('#personas');
+console.log(personas.rows.length);
+//var numFilas = tabla.rows.length;
+
+let nombre = document.querySelector('#nombre');
+let apellido = document.querySelector('#apellido');
+let celular = document.querySelector('#celular');
+
+
+function numFilas() {
+    let tabla = document.querySelector('#personas');
+    return  tabla.rows.length;
+}
+
+
+ function createRow() {
+
+    // let row = tabla.insertRow(this.numFilas);
+     let row = tabla.insertRow(numFilas());
+     console.log(this.numFilas);
+     let cell1 = row.insertCell(0);
+     let cell2 = row.insertCell(1);
+     let cell3 = row.insertCell(2);
+     let cell4 = row.insertCell(3);
+     let cell5 = row.insertCell(4);
+     let cell6 = row.insertCell(5);
+
+     cell1.setAttribute('scope','row')
+
+     cell1.innerHTML =`<b>${numFilas()+1}</b>`;
+     cell2.innerHTML = this.nombre.value;
+     cell3.innerHTML = this.apellido.value;
+     cell4.innerHTML = this.celular.value;
+     cell5.innerHTML = `<a href="#" class="btn btn-primary">Eliminar</a>`;
+     cell6.innerHTML =`<a href="#" class="btn btn-warning">Editar</a>`;
+     console.log(numFilas());
+
+    }
